dba3d377375b8f33abc324cdf680c539744680c5
Jaehyeok Choi <77jack0105@gmail.com>
2026-01-06 14:14:47 +0900

    Reduce whitespace

--- src/pages/CookiePage.tsx
+++ src/pages/CookiePage.tsx
@@ -9,7 +9,7 @@ export default function CookiePage() {
       transition={{ duration: 0.3 }}
     >
       {/* Hero Section */}
-      <section className="pt-32 pb-8 lg:pt-40 lg:pb-12 bg-gradient-to-b from-brand-50 to-white">
+      <section className="pt-32 pb-4 lg:pt-40 lg:pb-6 bg-gradient-to-b from-brand-50 to-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -31,7 +31,7 @@ export default function CookiePage() {
       </section>
 
       {/* Cookie Content Section */}
-      <section className="pt-8 pb-16 lg:pt-12 lg:pb-24 bg-white">
+      <section className="pt-4 pb-16 lg:pt-6 lg:pb-24 bg-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
